f8064ec248b67906c80782244663c03785fc9389
Nupur Jain <202152325@iiitvadodara.ac.in>
2024-06-27 23:46:15 +0530

    created otp model

---
+++ models/OTP.model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const OTPSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    otp: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, expires: 300 } // OTP expires in 5 minutes
+});
+
+const OTPModel = mongoose.model('OTP', OTPSchema);
+
+export default OTPModel;
