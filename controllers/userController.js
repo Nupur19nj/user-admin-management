ba447f3a8dac0ce122307e45b83c98af3f0714f5
Nupur Jain <202152325@iiitvadodara.ac.in>
2024-06-28 00:01:18 +0530

    initial userController

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+export const register = async (req, res) => {};
+
+export const verifyOTP = async (req, res) => {};
+
+export const login = async (req, res) => {};
+
+export const getUser = async (req, res) => {};
+
+export const updateUser = async (req, res) => {};
