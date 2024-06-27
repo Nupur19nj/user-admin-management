5c0ffaba0fe6b21540324617ff1bc8995e53731b
Nupur Jain <202152325@iiitvadodara.ac.in>
2024-06-27 23:30:04 +0530

    Setup MongoDB database connection in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -9,7 +10,18 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 app.use(express.json());
 
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Could not connect to MongoDB', err));
+
+const db = mongoose.connection;
+
 // Start server
 app.listen(PORT, () => {
-  console.log(`Nupur's Server is running at port ${PORT}`);
+    console.log(`Nupur's Server is running at port ${PORT}`);
 });
